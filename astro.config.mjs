e740d09da3e6ef8014ee8f60517d8b32bdd6b203
JonathanArroyaveGonzalez <arroyave3000g@gmail.com>
2024-03-29 03:05:49 -0500

    Modificacion componente content.astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,5 @@ import { defineConfig } from 'astro/config'
 export default defineConfig({
   site: 'https://JonathanArroyaveGonzalez.github.io',
   base: '/Regressions',
+  trailingSlash: "never",
 })
